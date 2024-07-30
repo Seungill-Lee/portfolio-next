b598640a0b97137e1d4c4a1b780434efca3c1f41
ever9415 <ever9415@mvint.co.kr>
2024-07-31 01:39:51 +0900

    테스트 해봅시다.

--- components/layout/header.js
+++ components/layout/header.js
@@ -15,20 +15,32 @@ export default function Header(props) {
     const pathname = usePathname();
     const router = useRouter();
     const slideLoading = props.slideLoadingIs;
+    const [vhSetting,completeVhSetting] = useState(false);
 
     useEffect(()=> {
-        const screenRatio = () => {
-            //console.log(window.innerWidth)
+        if(vhSetting == false) {
+            const setVh = () => {
+                const vh = window.innerHeight * 0.01;
+                document.documentElement.style.setProperty('--vh', `${vh}px`);
+            };
+            setVh();
+            window.addEventListener('resize orientationchange', setVh);
 
-            let winWidth = window.innerWidth;
-            let winHeight = window.innerHeight;
+            const screenRatio = () => {
+                //console.log(window.innerWidth)
 
-            document.documentElement.style.setProperty("--screen-ratio",Math.max(winWidth,winHeight)/Math.min(winWidth,winHeight))
+                let winWidth = window.innerWidth;
+                let winHeight = window.innerHeight;
+
+                document.documentElement.style.setProperty("--screen-ratio",Math.max(winWidth,winHeight)/Math.min(winWidth,winHeight))
+            }
+            screenRatio();
+            window.addEventListener('resize orientationchange', screenRatio);
+
+            completeVhSetting(true)
         }
-        screenRatio();
-        window.onresize = screen.onorientationchange = screenRatio
 
-    },[])
+    },[vhSetting])
 
     const [enterIs , stateEnterIs]  = useState(props.enterIs)
 
@@ -39,7 +51,7 @@ export default function Header(props) {
             <h1 className="hidden_cont">Seungill&apos;s Portfolio</h1>
             <p className={`${scss.intro} hidden_cont`}>Welcome to my portfolio page.</p>
             <div className={scss.btn_enter}>
-                <button type="button" onClick={()=> {stateEnterIs(true); router.push("/about")}} disabled={slideLoading || enterIs ? true : false}>&#123; Enter &#125;</button>
+                <button type="button" onClick={()=> {stateEnterIs(true); router.push("/about")}} disabled={!vhSetting || slideLoading || enterIs ? true : false}>&#123; Enter &#125;</button>
             </div>
         </header>
     )
